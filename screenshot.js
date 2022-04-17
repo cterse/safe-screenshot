fdb4519d984eb2173d15407a432f03e39ab1e594
Chinmay Terse <cterse@gmail.com>
2022-04-17 00:04:07 -0400

    Add try-catch block

--- screenshot.js
+++ screenshot.js
@@ -5,19 +5,27 @@ const fs = require('fs');
 const [, , ...args] = process.argv;
 
 (async () => {
-  const url = args[0];
-  const filename = `${args[1]}.png`;
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox']
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: 'networkidle0'
-  });
-  await page.screenshot({
-    path: filename,
-    fullPage: true
-  });
-  await page.close();
-  await browser.close();
+  try {
+    const url = args[0];
+    const filename = `${args[1]}.png`;
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+    });
+    await page.screenshot({
+      path: filename,
+      fullPage: true
+    });
+    await page.close();
+    await browser.close();
+  
+  } catch (error) {
+   
+    console.log(error);
+  
+  }
+  
 })();
